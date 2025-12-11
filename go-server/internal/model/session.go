6c65ab6efd8fcf49d900389def12c5196386b639
Direwen <direwensik@gmail.com>
2025-12-11 10:33:04 +0630

    fix: add is_duplicate field to session model

--- go-server/internal/model/session.go
+++ go-server/internal/model/session.go
@@ -6,4 +6,5 @@ type Session struct {
 	BaseModel
 	Demographics datatypes.JSON `gorm:"type:jsonb" json:"demographics"`
 	Fingerprint  string         `gorm:"type:varchar(255)" json:"fingerprint"`
+	IsDuplicate  bool           `gorm:"type:boolean;default:true" json:"is_duplicate"`
 }
