fbc78f1f91858cf8f344fa20dad64adb70785c6b
Direwen <direwensik@gmail.com>
2025-12-13 10:38:58 +0630

    feat: add basic helper queries of scenario and response repositories

---
+++ go-server/internal/repository/scenario.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/direwen/go-server/internal/model"
+	"gorm.io/gorm"
+)
+
+type ScenarioRepository interface {
+	GetLatestUnanswered(ctx context.Context, sessionID string) (*model.Scenario, error)
+}
+
+type scenarioRepository struct {
+	db *gorm.DB
+}
+
+func NewScenarioRepository(db *gorm.DB) ScenarioRepository {
+	return &scenarioRepository{db}
+}
+
+func (r *scenarioRepository) GetLatestUnanswered(ctx context.Context, sessionID string) (*model.Scenario, error) {
+	var scenario model.Scenario
+
+	err := r.db.WithContext(ctx).
+		Joins("LEFT JOIN responses ON scenarios.id = responses.scenario_id AND responses.session_id = ?", sessionID).
+		Where("responses.id IS NULL").
+		Order("scenarios.created_at DESC").
+		First(&scenario).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &scenario, nil
+}
